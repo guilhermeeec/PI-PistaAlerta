7de36e40358bb534053d3cce2db1932c4e251dc3
Guilherme Araujo Thomaz <guilhermepic2016@gmail.com>
2022-10-03 19:24:20 -0300

    Arquivos para testar módulo RF 433MHz ASK

---
+++ teste_tx/teste_tx.ino
@@ -0,0 +1,24 @@
+#include <RH_ASK.h>
+#include <SPI.h> //INCLUSÃO DE BIBLIOTECA
+
+RH_ASK driver; //CRIA O DRIVER PARA COMUNICAÇÃO
+
+const int pinoBotao = 3; //PINO DIGITAL UTILIZADO PELO PUSH BUTTON
+
+void setup(){
+    driver.init(); //INICIALIZA A COMUNICAÇÃO RF DO DRIVER
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(pinoBotao, INPUT); //DEFINE O PINO COMO ENTRADA / "_PULLUP" É PARA ATIVAR O RESISTOR INTERNO
+    //DO ARDUINO PARA GARANTIR QUE NÃO EXISTA FLUTUAÇÃO ENTRE 0 (LOW) E 1 (HIGH)
+}
+void loop(){
+    const char *msg = "led"; //VARIÁVEL RECEBE O VALOR (led)
+
+    if(digitalRead(pinoBotao) == LOW){ //SE A LEITURA DO PINO FOR IGUAL A LOW, FAZ
+        digitalWrite(LED_BUILTIN, HIGH);
+        driver.send((uint8_t *)msg, strlen(msg)); //ENVIA AS INFORMAÇÕES PARA O RECEPTOR (PALAVRA: led)
+        driver.waitPacketSent(); //AGUARDA O ENVIO DAS INFORMAÇÕES
+        delay(200); //INTERVALO DE 200 MILISSEGUNDOS
+    }
+    digitalWrite(LED_BUILTIN, LOW);
+}
